c615022ca90e6f924162f760b2992e64e694dfdd
JGPico <minq_2@hotmail.com>
2020-08-06 15:28:21 -0600

    word count done

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,10 +1,27 @@
+unwanted_chars = ["\"", ":", ";", ",", ".", "-", "+", "=",
+                  "/", "\\", "|", "[", "]", "{", "}", "(", ")", "*", "^", "&"]
+
+
 def word_count(s):
     # Your code here
+    dict_of_words = {}
 
+    for word in s.split():
+        for i in unwanted_chars:
+            word = word.replace(i, '')
+        if word.lower() in dict_of_words:
+            dict_of_words[word.lower()] += 1
+        else:
+            dict_of_words[word.lower()] = 1
+
+    if len(dict_of_words) == 1 and '' in dict_of_words:
+        dict_of_words = {}
+    return dict_of_words
 
 
 if __name__ == "__main__":
     print(word_count(""))
     print(word_count("Hello"))
     print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
-    print(word_count('This is a test of the emergency broadcast network. This is only a test.'))
+    print(word_count(
+        'This is a test of the emergency broadcast network. This is only a test.'))
